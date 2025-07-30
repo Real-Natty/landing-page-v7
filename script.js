78db189a114dd1890787e11513e1b08740421e04
Chibuike <emmanueleluagu24@gmail.com>
2025-07-30 15:48:28 +0100

    'new'

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const elementOne = document.getElementById("nav");
 const elementTwo = document.getElementById("hamburger-container");
+const footer = document.getElementById("footer");
+const footerSpan = footer.querySelector("#footer-span");
+const year = new Date().getFullYear();
+
+footerSpan.innerHTML = `&copy; ${year}`;
 
 elementTwo.addEventListener("click", function () {
   elementOne.classList.toggle("show");
